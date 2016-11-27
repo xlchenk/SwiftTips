32b663148e48a1f1b92530244209979551b27fe7
陈晓龙 <chenxl@chenxl.local>
2016-11-27 15:53:45 +0800

    Initial Commit

---
+++ SwiftTips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTips
+//
+//  Created by 陈晓龙 on 16/11/27.
+//  Copyright © 2016年 陈晓龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
